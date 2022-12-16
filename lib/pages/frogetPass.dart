4c5d23902938a2ab9f7df3f9d2b05f5b9e5b93ba
Ayan Gorain <ayan.gorain2021@vitbhopal.ac.in>
2022-12-16 13:47:46 +0530

    forget password page

--- lib/pages/frogetPass.dart
+++ lib/pages/frogetPass.dart
@@ -19,44 +19,63 @@ class forgetpass extends StatelessWidget {
       ),
         backgroundColor: Colors.white,
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                child: Image.asset('assets/images/forPass.png'),
-                width: 300,
-              ),
-              Text('Please Enter Your Email Address To\n     Recevic a Verification Code',
-              style: TextStyle(
-                  color: Colors.teal[900],
-                  fontSize: 20,
-                  fontFamily: 'Poppins Pro'
-              ),),
-              Text('Email Address -',
-                  style: TextStyle(
-                    color: Colors.blueGrey[300] ,
-                    fontSize: 17,),),
-              Container(
-                  color: Colors.blueGrey[100],
-                  padding: EdgeInsets.all(0.2),
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 27),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.mail,
-                      color: Colors.red[400],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Image.asset('assets/images/forPass.png'),
+                  width: 300,
+                ),
+                Text('Please Enter Your Email Address To\n     Recevic a Verification Code',
+                style: TextStyle(
+                    color: Colors.teal[900],
+                    fontSize: 20,
+                    fontFamily: 'Poppins Pro'
+                ),),
+                SizedBox(height: 30,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30,right: 30),
+                  child: TextFormField(
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "email cannot be empty";
+                      }
+                      return null;
+
+                    },
+                    //onChanged: (value){
+                   //   _email=value.trim();
+                   // },
+                    decoration: InputDecoration(
+                      prefixIcon : Icon(Icons.mail_outline),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        borderSide: BorderSide(color: Colors.blue),),
+                      labelText: 'Email',
+                      hintText: 'Enter Email here',
                     ),
-                    title: Text('example@gmail.com',
-                        style: TextStyle(
-                          fontSize: 19,
-                          color: Colors.blueGrey[400],
-                          fontFamily: 'Source Sans Pro',
-                        )
-                    ),
-                  )
-              )
+                  ),
+                ),
+                SizedBox(height: 20,),
+                MaterialButton(
+                  minWidth:250,
+                  height: 60,
+                  onPressed: ()  {  },
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Text("get otp",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      )),
+                ),
 
 
-            ],
+              ],
+            ),
           ),
         ));
   }
